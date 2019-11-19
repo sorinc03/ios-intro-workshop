47681dd21f331439f16f630810223a8cdb90e091
Sorin Cioban <scioban@twitter.com>
2019-11-19 09:56:46 +0000

    Initial Commit

---
+++ intro-to-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intro-to-ios
+//
+//  Created by Sorin Cioban on 19/11/2019.
+//  Copyright © 2019 Sorin Cioban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ intro-to-iosTests/intro_to_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  intro_to_iosTests.swift
+//  intro-to-iosTests
+//
+//  Created by Sorin Cioban on 19/11/2019.
+//  Copyright © 2019 Sorin Cioban. All rights reserved.
+//
+
+import XCTest
+@testable import intro_to_ios
+
+class intro_to_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
