ddb20fbd91a008e17456d82ba6b4b68b7f3126ea
Sorin Cioban <scioban@twitter.com>
2019-11-20 10:26:20 +0000

    Add Puppy, Puppies, PuppyManager skeleton

---
+++ intro-to-ios/Puppies.swift
@@ -0,0 +1,21 @@
+//
+//  Puppies.swift
+//  intro-to-ios
+//
+//  Created by Sorin Cioban on 20/11/2019.
+//  Copyright © 2019 Sorin Cioban. All rights reserved.
+//
+
+import Foundation
+
+struct Puppy {
+
+}
+
+struct Puppies {
+
+}
+
+class PuppyManager {
+    
+}
