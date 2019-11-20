ea4835fec9d0e354cf49a987353e86a33ebf1393
Sorin Cioban <scioban@twitter.com>
2019-11-20 15:31:58 +0000

    Complete puppy exercise

--- intro-to-ios/Puppies.swift
+++ intro-to-ios/Puppies.swift
@@ -8,14 +8,35 @@
 
 import Foundation
 
-struct Puppy {
+struct Puppy: Codable {
+    var name: String?
+    var imageName: String?
 
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "picture"
+    }
 }
 
-struct PuppyResponse {
-
+struct PuppyResponse: Codable {
+    var puppies: [Puppy]?
 }
 
 class PuppyManager {
-    
+    var puppies: [Puppy]?
+    init?() {
+        guard let url = Bundle.main.url(forResource: "puppies", withExtension: "json") else {
+            fatalError("Could not find puppies JSON file")
+            return nil
+        }
+
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let puppyResponse = try decoder.decode(PuppyResponse.self, from: data)
+            puppies = puppyResponse.puppies
+        } catch {
+            print("Could not get valid data \(error)")
+        }
+    }
 }
--- intro-to-ios/ViewController.swift
+++ intro-to-ios/ViewController.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class PuppyCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    var puppy: Puppy? {
+        didSet {
+            imageView.image = UIImage(named: puppy?.imageName ?? "")
+        }
     }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([contentView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor), contentView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor), contentView.topAnchor.constraint(equalTo: imageView.topAnchor), contentView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor)])
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+class ViewController: UIViewController, UICollectionViewDataSource {
+    var collectionView: UICollectionView?
+    let puppyCellIdentifier = "puppyCellIdentifier"
+    var puppies: [Puppy]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Puppies"
+        view.backgroundColor = .white
+
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 200, height: 200)
+        flowLayout.minimumLineSpacing = 10.0
+        flowLayout.minimumInteritemSpacing = 10.0
+
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.translatesAutoresizingMaskIntoConstraints = false
+        collectionView?.dataSource = self
+        collectionView?.register(PuppyCell.self, forCellWithReuseIdentifier: puppyCellIdentifier)
+        collectionView?.backgroundColor = .clear
+        guard let collectionView = collectionView else {
+            return
+        }
+
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor), view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: collectionView.topAnchor), view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor), view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor)])
+
+        if let puppyObjects = PuppyManager()?.puppies {
+            puppies = puppyObjects
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return puppies.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: puppyCellIdentifier, for: indexPath) as! PuppyCell
+        cell.puppy = puppies[indexPath.item]
+        return cell
+    }
 }
 
